50c84dfafd995f649a42185e75a910a397902721
sechiyo97 <sechiyo97@gmail.com>
2022-01-04 03:26:34 +0900

    initial commit

---
+++ app/src/main/java/com/studiowash/mumong_andorid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.studiowash.mumong_andorid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
