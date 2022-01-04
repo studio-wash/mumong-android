ef02bfc64c90d48da55707ced48eccd6efce9574
sechiyo97 <sechiyo97@gmail.com>
2022-01-04 23:36:48 +0900

    apply package name to omitted files

--- app/src/main/java/com/studiowash/mumong/MainActivity.kt
+++ app/src/main/java/com/studiowash/mumong/MainActivity.kt
@@ -8,6 +8,7 @@ import com.studiowash.mumong.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
