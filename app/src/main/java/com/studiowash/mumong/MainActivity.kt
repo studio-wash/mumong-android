11b0951fcab922249164e0cb24dbf77a1d40dc37
sechiyo97 <sechiyo97@gmail.com>
2022-02-10 01:58:29 +0900

    turn bottom navigation ripple effect off

--- app/src/main/java/com/studiowash/mumong/MainActivity.kt
+++ app/src/main/java/com/studiowash/mumong/MainActivity.kt
@@ -3,7 +3,6 @@ package com.studiowash.mumong
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.studiowash.mumong.databinding.ActivityMainBinding
@@ -19,9 +18,9 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun initNavigation() {
+        binding.bottomNavigationView.itemIconTintList = null
         supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.findNavController()?.let { navController ->
             binding.bottomNavigationView.setupWithNavController(navController)
-
         }
     }
 }
