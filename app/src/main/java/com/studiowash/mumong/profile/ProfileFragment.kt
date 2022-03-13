41cb704addc07f5e24b244f1772718ae8ac6ca01
sechiyo97 <sechiyo97@daum.net>
2022-03-13 16:22:54 +0900

    add setting item list to profile fragment

---
+++ app/src/main/java/com/studiowash/mumong/profile/ProfileFragment.kt
@@ -0,0 +1,28 @@
+package com.studiowash.mumong.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.studiowash.mumong.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        initView()
+        initOnClick()
+        return binding.root
+    }
+
+    private fun initView() {
+    }
+
+    private fun initOnClick() {
+    }
+}
