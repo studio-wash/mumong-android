bc2b25c3ffedc6ce910bb3eff2c05482d5bec626
sechiyo97 <sechiyo97@daum.net>
2022-04-19 03:15:34 +0900

    decorate add practice fragment

--- presentation/src/main/java/com/studiowash/mumong/presentation/activity/practice/addpractice/AddPracticeFragment.kt
+++ presentation/src/main/java/com/studiowash/mumong/presentation/activity/practice/addpractice/AddPracticeFragment.kt
@@ -13,11 +13,5 @@ class AddPracticeFragment : Fragment(R.layout.fragment_practice_add_practice) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentPracticeAddPracticeBinding.bind(view)
-
-        initToolbar()
-    }
-
-    private fun initToolbar() {
-        binding.toolbar.inflateMenu(R.menu.toolbar_action_menu_add_practice)
     }
 }
---
+++ presentation/src/main/java/com/studiowash/mumong/presentation/activity/practice/addpractice/MetronomeTunerStatus.kt
@@ -0,0 +1,3 @@
+package com.studiowash.mumong.presentation.activity.practice.addpractice
+
+enum class MetronomeTunerStatus { Metronome, Tuner, None }
