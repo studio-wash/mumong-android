f718b072a71c052f86f0785426d027d1cb8afe42
sechiyo97 <sechiyo97@daum.net>
2022-05-05 02:11:58 +0900

    change images

--- presentation/src/main/java/com/studiowash/mumong/presentation/activity/social/friend/SocialHomeFriendFragment.kt
+++ presentation/src/main/java/com/studiowash/mumong/presentation/activity/social/friend/SocialHomeFriendFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.studiowash.mumong.domain.Constants
 import com.studiowash.mumong.domain.common.entity.RecordingEntity
 import com.studiowash.mumong.domain.social.entity.OnlineFriendEntity
 import com.studiowash.mumong.domain.social.entity.SocialArticleEntity
@@ -26,58 +27,58 @@ class SocialHomeFriendFragment : Fragment() {
             OnlineFriendEntity(
                 UserEntity(
                     nickname = "데이드림",
-                    profileImg = "https://whoisnerdy.com/web/product/big/202201/0cb0fe62aac7685c3692371492c2cbeb.png"
+                    profileImg = Constants.sample_image_url
                 ), true
             ),
             OnlineFriendEntity(
                 UserEntity(
                     nickname = "비지비지",
-                    profileImg = "https://whoisnerdy.com/web/product/big/202201/0cb0fe62aac7685c3692371492c2cbeb.png"
+                    profileImg = Constants.sample_image_url
                 ), true
             ),
             OnlineFriendEntity(
                 UserEntity(
                     nickname = "까지",
-                    profileImg = "https://whoisnerdy.com/web/product/big/202201/0cb0fe62aac7685c3692371492c2cbeb.png"
+                    profileImg = Constants.sample_image_url
                 ), true
             ),
             OnlineFriendEntity(
                 UserEntity(
                     nickname = "무수한 연습",
-                    profileImg = "https://whoisnerdy.com/web/product/big/202201/0cb0fe62aac7685c3692371492c2cbeb.png"
+                    profileImg = Constants.sample_image_url
                 ), false
             ),
             OnlineFriendEntity(
                 UserEntity(
                     nickname = "샤샤샤",
-                    profileImg = "https://whoisnerdy.com/web/product/big/202201/0cb0fe62aac7685c3692371492c2cbeb.png"
+                    profileImg = Constants.sample_image_url
                 ), false
             ),
             OnlineFriendEntity(
                 UserEntity(
                     nickname = "데이이드림",
-                    profileImg = "https://whoisnerdy.com/web/product/big/202201/0cb0fe62aac7685c3692371492c2cbeb.png"
+                    profileImg = Constants.sample_image_url
                 ), false
             )
         )
 
         articles = listOf(
-            com.studiowash.mumong.domain.social.entity.SocialArticleEntity(
+            SocialArticleEntity(
                 "오늘은  레슨실에서 녹턴 피아노 연습!\n통기타 2주차 연습곡으로 10월의 어느 멋진 날 연습했다! 코드 잡는 법이 아직 어렵다.",
                 "1분 전",
                 84, 24,
                 UserEntity(
                     nickname = "데이드림",
-                    profileImg = "https://whoisnerdy.com/web/product/big/202201/0cb0fe62aac7685c3692371492c2cbeb.png"
+                    profileImg = Constants.sample_image_url
                 ),
                 recordings = listOf(
-                    com.studiowash.mumong.domain.common.entity.RecordingEntity(
+                    RecordingEntity(
                         "none",
                         "1:34",
                         "피아노",
                         "녹턴 Op.9-2 (쇼팽)"
                     ),
-                    com.studiowash.mumong.domain.common.entity.RecordingEntity(
+                    RecordingEntity(
                         "none",
                         "1:34",
                         "통기타",
@@ -85,43 +86,43 @@ class SocialHomeFriendFragment : Fragment() {
                     )
                 )
             ),
-            com.studiowash.mumong.domain.social.entity.SocialArticleEntity(
+            SocialArticleEntity(
                 "바빠서 오늘 연습은 패스",
                 "2시간 전",
                 3, 2,
                 UserEntity(
                     nickname = "비지비지",
-                    profileImg = "https://whoisnerdy.com/web/product/big/202201/0cb0fe62aac7685c3692371492c2cbeb.png"
+                    profileImg = Constants.sample_image_url
                 )
             ),
-            com.studiowash.mumong.domain.social.entity.SocialArticleEntity(
+            SocialArticleEntity(
                 "새로운 악기를 배우는 것은 언제나 즐겁다!\n오늘 처음 시작한 콘트라베이스도 굉장히 매력있는 악기인 듯 한데 아니 무슨 콘트라베이스를 갑자기 배우나요 ㅋㅋ",
                 "1일 전",
                 245, 121,
                 UserEntity(
                     nickname = "까지",
-                    profileImg = "https://whoisnerdy.com/web/product/big/202201/0cb0fe62aac7685c3692371492c2cbeb.png"
+                    profileImg = Constants.sample_image_url
                 ),
                 recordings = listOf(
-                    com.studiowash.mumong.domain.common.entity.RecordingEntity(
+                    RecordingEntity(
                         "none",
                         "1:34",
                         "피아노",
                         "녹턴 Op.9-(쇼팽)"
                     ),
-                    com.studiowash.mumong.domain.common.entity.RecordingEntity(
+                    RecordingEntity(
                         "none",
                         "1:34",
                         "콘트라베이스",
                         "10월의 어느 멋진 날"
                     ),
-                    com.studiowash.mumong.domain.common.entity.RecordingEntity(
+                    RecordingEntity(
                         "none",
                         "1:34",
                         "드럼",
                         "BEAT 120"
                     ),
-                    com.studiowash.mumong.domain.common.entity.RecordingEntity(
+                    RecordingEntity(
                         "none",
                         "1:34",
                         "통기타",
