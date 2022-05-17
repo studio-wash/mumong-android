c3b285a597c0225359d2662f4a76c2a2e6b8ea1f
sechiyo97 <sechiyo97@daum.net>
2022-05-18 01:31:07 +0900

    add room dependency to data layer

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -62,5 +62,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 
+    // room
+    val roomVersion = "2.4.2"
+    implementation("androidx.room:room-ktx:$roomVersion")
+
     implementation(project(":domain"))
 }
