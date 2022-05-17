3073ff3f0fdd79eb49653ba2bfed5e8eaa2ff05c
sechiyo97 <sechiyo97@daum.net>
2022-05-18 03:25:16 +0900

    add more dependencies for room

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -42,14 +42,14 @@ dependencies {
 
     // view
     implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.5.0")
+    implementation("com.google.android.material:material:1.6.0")
 
     // serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 
     // retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
 
     // hilt
@@ -64,7 +64,8 @@ dependencies {
 
     // room
     val roomVersion = "2.4.2"
-    implementation("androidx.room:room-ktx:$roomVersion")
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
 
     implementation(project(":domain"))
 }
