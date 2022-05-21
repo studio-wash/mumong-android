2baf73f0889bdbec472ce0bb8d7ae36b75e78652
sechiyo97 <sechiyo97@daum.net>
2022-05-21 21:19:15 +0900

    apply major revision of figma layout for profile main fragment

--- presentation/src/main/java/com/studiowash/mumong/presentation/screen/main/MainProfileFragment.kt
+++ presentation/src/main/java/com/studiowash/mumong/presentation/screen/main/MainProfileFragment.kt
@@ -1,14 +1,12 @@
 package com.studiowash.mumong.presentation.screen.main
 
-import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.studiowash.mumong.presentation.screen.MumongFragment
-import com.studiowash.mumong.presentation.constant.StringKeySet
-import com.studiowash.mumong.presentation.constant.StringValueSet
+import com.studiowash.mumong.domain.login.LoginStatus
 import com.studiowash.mumong.presentation.databinding.FragmentMainProfileBinding
+import com.studiowash.mumong.presentation.screen.MumongFragment
 
 class MainProfileFragment : MumongFragment(true) {
     private lateinit var binding: FragmentMainProfileBinding
@@ -24,28 +22,12 @@ class MainProfileFragment : MumongFragment(true) {
     }
 
     private fun initView() {
-        binding.user = com.studiowash.mumong.domain.login.LoginStatus.currentUser
-        binding.root.viewTreeObserver.addOnGlobalLayoutListener {
-            initScrollY()
-        }
+        binding.user = LoginStatus.currentUser
         binding.ivProfile.clipToOutline = true
+
+        binding.showAlertRedDot = true
     }
 
     private fun initOnClick() {
-    }
-
-    private fun initScrollY() {
-        val titleView = when (activity?.intent?.getStringExtra(StringKeySet.CATEGORY)) {
-            StringValueSet.SOCIAL -> binding.titleSocialCategoryTextView
-            StringValueSet.COMMUNITY -> binding.titleCommunityCategoryTextView
-            else -> return
-        }
-
-        val offsetViewBounds = Rect()
-        titleView.getDrawingRect(offsetViewBounds)
-        binding.itemsScrollView.offsetDescendantRectToMyCoords(titleView, offsetViewBounds)
-        val relativeTop: Int = offsetViewBounds.top
-
-        binding.itemsScrollView.scrollTo(0, relativeTop - 150)
     }
 }
