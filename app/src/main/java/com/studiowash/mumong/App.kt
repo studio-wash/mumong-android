a19f2a37e373f97b8c81c23c5a48b34d46261d0f
sechiyo97 <sechiyo97@daum.net>
2022-05-26 23:59:36 +0900

    add kakao native app key

--- app/src/main/java/com/studiowash/mumong/App.kt
+++ app/src/main/java/com/studiowash/mumong/App.kt
@@ -1,7 +1,14 @@
 package com.studiowash.mumong
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+    }
+}
